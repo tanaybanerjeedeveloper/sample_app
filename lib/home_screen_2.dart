1ffe1c25f657d8decd27e78f43192516236e91a1
Tanay Banerjee <tanaybanerjee@Tanays-iMac.local>
2024-02-23 16:06:35 +0530

    initial commit

--- lib/home_screen_2.dart
+++ lib/home_screen_2.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:net_app/providers/home_screen_provider.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:net_app/widgets/common_button.dart';
 import 'package:net_app/widgets/todo_widget.dart';
 
-class HomeScreen extends ConsumerWidget {
-  const HomeScreen({super.key});
+class HomeScreenTwo extends StatefulWidget {
+  const HomeScreenTwo({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final homePvd = ref.watch(homeProvider);
+  State<HomeScreenTwo> createState() => _HomeScreenTwoState();
+}
+
+class _HomeScreenTwoState extends State<HomeScreenTwo> {
+  bool _isLoading = false;
+  List _data = [];
+
+  getData() async {
+    try {
+      setState(() {
+        _isLoading = true;
+      });
+
+      var response = await http
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+      _data = json.decode(response.body);
+      print('response: $_data');
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        backgroundColor: Colors.red,
+        content: Text('Something went wrong, please try again later'),
+      ));
+      print('error');
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
@@ -22,11 +53,9 @@ class HomeScreen extends ConsumerWidget {
               children: [
                 SizedBox(height: 20),
                 CommonButton(
-                  color:
-                      (homePvd.isLoading) ? Colors.grey.shade400 : Colors.blue,
-                  onPressed:
-                      (homePvd.isLoading) ? null : () => homePvd.getData(),
-                  widget: (homePvd.isLoading)
+                  color: (_isLoading) ? Colors.grey.shade400 : Colors.blue,
+                  onPressed: (_isLoading) ? null : () => getData(),
+                  widget: (_isLoading)
                       ? SizedBox(
                           width: 20,
                           height: 20,
@@ -39,9 +68,9 @@ class HomeScreen extends ConsumerWidget {
                           style: TextStyle(color: Colors.white),
                         ),
                 ),
-                (homePvd.data.isEmpty && homePvd.isLoading == false)
+                (_data.isEmpty && _isLoading == false)
                     ? SizedBox()
-                    : (homePvd.isLoading)
+                    : (_isLoading)
                         ? Column(
                             children: [
                               SizedBox(),
@@ -50,10 +79,10 @@ class HomeScreen extends ConsumerWidget {
                         : ListView.builder(
                             shrinkWrap: true,
                             physics: NeverScrollableScrollPhysics(),
-                            itemCount: homePvd.data.length,
+                            itemCount: _data.length,
                             itemBuilder: (context, index) => TodoWidget(
-                                isCompleted: homePvd.data[index]['completed'],
-                                title: homePvd.data[index]['title']))
+                                isCompleted: _data[index]['completed'],
+                                title: _data[index]['title']))
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:net_app/home_screen.dart';
+import 'package:net_app/home_screen_2.dart';
 
 void main() {
   runApp(ProviderScope(child: const MyApp()));
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: HomeScreenTwo(),
     );
   }
 }
